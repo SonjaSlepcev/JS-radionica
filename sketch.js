e9b5267316a1a768acbfa8f0f442cf8c067c25d2
Sonja Slepčev <sonja.slepcev@namics.com>
2019-02-02 12:11:11 +0100

    Create eyes

--- sketch.js
+++ sketch.js
@@ -1,9 +1,13 @@
 function setup() {
-  createCanvas(400, 300);
-  background(200);
+ createCanvas(400, 300);
+ background(200);
 
-  strokeWeight(4);
+ strokeWeight(4);
 
-  fill(0,170,0);
-  rect(width/2-50, height/2-50, 100, 100);
+ fill(0,170,0);
+ rect(width/2-50, height/2-50, 100, 100);
+
+  fill(255);
+ ellipse(width/2-50+25, height/2-50+30, 20, 20)
+ ellipse(width/2-50+70, height/2-50+30, 30, 30)
 }
